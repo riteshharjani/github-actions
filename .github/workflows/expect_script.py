69f1f1f0886040293d4ffefc3a00b65489f68a00
Ritesh Harjani (IBM) <ritesh.list@gmail.com>
2023-04-21 06:25:52 +0530

    self hosted runner
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>

--- .github/workflows/expect_script.py
+++ .github/workflows/expect_script.py
@@ -21,8 +21,9 @@ cloud_image_path = './jammy-server-cloudimg-amd64.img'
 #                 -device virtio-net,netdev=vmnic \
 #                 -netdev user,id=vmnic,hostfwd=tcp::12125-:22 \
 # Create a pexpect object to run the QEMU command
-qemu_command = f'''qemu-system-x86_64 -enable-kvm \
-                 -smp 4 \
+qemu_command = f'''qemu-system-x86_64 \
+                 -enable-kvm \
+                 -smp 2 \
                  -nographic \
                  -m 8192 \
                  -kernel arch/x86/boot/bzImage \
